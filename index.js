9488fafee092b9ed207aee8f98496f4c167cbf2a
Moses <mochengo29@gmail.com>
2023-06-12 18:15:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
   // Write your code here!
+  if(num>2500){
+    return "No can do."
+  }
+  else if(num>2000){
+    return "I will gladly take your thirty bucks.";
+  }
+  else if(num<400){
+    return "This one is on me!";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case "generous":
+      return "Thank you so much."
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+  }
 }
